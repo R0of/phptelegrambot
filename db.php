795c081a171897575883b25940b15175131157b2
R00f <farrux301@mail.ru>
2020-06-05 18:39:24 +0500

    Create db.php
    Connect with DB

---
+++ db.php
@@ -0,0 +1,42 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+date_default_timezone_set('Asia/Tashkent');
+////////////Baza bilan bog'lanamiz
+require_once ("corebot.php");
+
+class Database { 
+	private $db;
+	private $senda;
+    public function __construct(){
+    	$this->db = new mysqli("localhost","usernameDB","passwordDB","nameDB");
+    	$this->senda = new sendtoadmin();
+    	
+    	if(mysqli_connect_errno()){
+    		$this->senda->sendadmin($data = ['text'=>"Error with DB:"
+    		. mysqli_connect_errno()], $chat_id, "sendMessage" );
+         	exit;
+       	}
+    }
+    public function isRegistered($user_id) {
+        $query = "SELECT * FROM user WHERE user_id = $user_id";
+        $result = $this->db->query($query);
+        $num_results = $result->num_rows;
+        if ($num_results>0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function register($user_id,$username,$first_name,$last_name) {
+    	$date = date("Y-m-d H:i:s");
+        $stmt = $this->db->prepare("INSERT INTO user ( user_id,username,first_name,last_name,created) 
+        VALUES (?,?,?,?,?)");
+        $stmt->bind_param("sssss",$user_id,$username,$first_name,$last_name,$date); 
+        $result = $stmt->execute();
+        if (!$result) {
+            return false;
+        } else {
+            return true;
+        }
+        $stmt->close();
+    } 
